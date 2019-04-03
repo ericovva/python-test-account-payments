1f29a37d0860d3ee85bed136ad7a845051122793
e.ovsepyan <e.ovsepyan@eovsepyan.local>
2019-04-03 20:38:23 +0300

    add management command, delete sqlite ->go to psql on docker

--- fintech_startup/fintech_startup/settings.py
+++ fintech_startup/fintech_startup/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'fintech_startup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'transfers',
+        'USER' : 'steve',
+        'HOST' : 'ps01',
+        'PORT' : '5432',
     }
 }
 
